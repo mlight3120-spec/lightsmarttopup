82cd48be62a64af0dd0ba97080b33006936d41dd
mlight3120-spec <mlight3120@gmail.com>
2025-09-06 07:28:28 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$config = include __DIR__ . '/config.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LightsmartTopup</title>
+</head>
+<body>
+    <h1>Welcome to LightsmartTopup</h1>
+    <p>Your reliable platform for Airtime & Data Topup.</p>
+    <a href="install.php">Run Installer</a>
+</body>
+</html>
