57e1d8a110cbcfd5cf6f4330179707162ea229fe
mlight3120-spec <mlight3120@gmail.com>
2025-09-08 10:03:29 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 return [
     // PostgreSQL Database Credentials (from Railway "Connect" tab)
-    'DB_HOST' => 'postgres.railway.internal',  // r
-    'DB_PORT' => '5432',                               // PGPORT (usually 5432)
+    'DB_HOST' => 'postgres.railway.internal',          // PGHOST
+    'DB_PORT' => '5432',                               // PGPORT 
     'DB_NAME' => 'railway',                            // PGDATABASE
     'DB_USER' => 'postgres',                           // PGUSER
     'DB_PASS' => 'zSXozaYXSwFVKyvUyebkGXUqezeqfqMf',  // PGPASSWORD
