3838671284784a0fd42518b92c0179a59933bf41
mlight3120-spec <mlight3120@gmail.com>
2025-09-10 11:48:45 +0100

    Update update_pin_column.php

--- update_pin_column.php
+++ update_pin_column.php
@@ -1,10 +1,11 @@
 <?php
 require 'config.php';
 
-try {
-    // Check if $conn exists
-    if (!isset($conn)) {
-        throw new Exception("Database connection not available. Check config.php.");
+    try {
+    $dsn = "pgsql:host={$config['DB_HOST']};port={$config['DB_PORT']};dbname={$config['DB_NAME']};";
+    $pdo = new PDO($dsn, $config['DB_USER'], $config['DB_PASS'], [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
     }
 
     $sql = "ALTER TABLE users ADD COLUMN pin TEXT NULL";
